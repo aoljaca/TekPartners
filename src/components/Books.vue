b3edf8da729b9eea0615a2edf3fe21282f43ea4d
andrej oljaca <andrejoljaca@andrejs-mbp.lan>
2022-04-11 09:26:56 -0400

    Tweaks

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,20 +1,101 @@
 <template>
   <div>
     <v-row>
-      <v-col v-for="(book, i) in books" :key="i" :cols="!showCondensedVersion ? 4 : 12">
-        <v-card @click="takeToBook(book)" class="d-flex flex-column align-center justify-center" height="400">
-          <v-img
-            :src="book.coverImageUrl"
-            height="200px"
-          ></v-img>
+      <v-col>
+        <v-text-field
+          label="Search"
+          single-line
+          hide-details
+          dense
+          prepend-icon="mdi-magnify"
+          v-model="searchBook"
+          @keyup="updateAfterSearch"
+        />
+      </v-col>
+      <v-col cols="auto">
+        <v-dialog v-model="filterDialog" max-width="290" class="bg-green">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn class="bg-green" dark v-bind="attrs" v-on="on">
+              Filters
+            </v-btn>
+          </template>
+          <v-card
+            class="set-opactiy bg-green font-weight-bold overflow-auto"
+            dark
+            height="350"
+          >
+            <div class="pa-4 d-flex align-center display-center">
+              <v-range-slider
+                hint="Years"
+                min="1800"
+                max="2000"
+                v-model="bookYearRange"
+              ></v-range-slider>
+              {{ bookYearRange }}
+            </div>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col
+        v-for="(book, i) in historyList"
+        :key="i"
+        :cols="!showCondensedVersion ? 4 : 12"
+      >
+        <v-card
+          @click="takeToBook(book)"
+          class="d-flex flex-column align-center justify-center"
+          height="400"
+        >
+          <v-img :src="book.coverImageUrl" height="200px"></v-img>
 
-          <v-card-title>{{book.title}}</v-card-title>
+          <v-tooltip
+            bottom
+            :open-on-click="showCondensedVersion"
+            :open-on-hover="!showCondensedVersion"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-card-title
+                v-bind="attrs"
+                v-on="isOverflown() ? on : undefined"
+                ref="roomName"
+                class="truncate font-weight-600"
+                >{{ book.title }}</v-card-title
+              >
+            </template>
+            <span>{{ book.title }}</span>
+          </v-tooltip>
 
-          <v-card-subtitle> Publisher: {{book.publisher}} </v-card-subtitle>
+          <v-tooltip
+            bottom
+            :open-on-click="showCondensedVersion"
+            :open-on-hover="!showCondensedVersion"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-card-subtitle
+                v-bind="attrs"
+                v-on="isOverflown() ? on : undefined"
+                ref="roomName"
+                class="truncate font-weight-600"
+              >
+                Publisher: {{ book.publisher }}
+              </v-card-subtitle>
+            </template>
+            <span>{{ book.publisher }}</span>
+          </v-tooltip>
 
           <v-card-text> </v-card-text>
         </v-card>
       </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-pagination
+        class="pagination mb-15"
+        v-model="page"
+        :length="pages"
+        @input="updatePage"
+      ></v-pagination>
     </v-row>
   </div>
 </template>
@@ -26,11 +107,65 @@ import { Book } from "../model/book";
 @Component
 export default class MainHeader extends Vue {
   books: Book[] = books;
-  takeToBook(book: Book): void {
-      this.$router.push({name: "Book", params: {id: book.id.toString()}})
+  searchBook = "";
+  page = 1;
+  pageSize = 6;
+  listCount = 0;
+  historyList: any[] = [];
+  bookYearRange = [1800, 2000];
+
+  get pages() {
+    if (this.pageSize == null || this.listCount == null) return 0;
+    return Math.ceil(this.listCount / this.pageSize);
   }
   get showCondensedVersion(): boolean {
     return this.$vuetify.breakpoint.smAndDown;
+  }
+  get filteredBooks(): any[] {
+    const toReturn: any[] = [];
+    this.books.forEach((b) => {
+      if (
+        b.title.toLowerCase().startsWith(this.searchBook.toLowerCase()) &&
+        b.year > this.bookYearRange[0] &&
+        b.year < this.bookYearRange[1]
+      ) {
+        toReturn.push(b);
+      }
+    });
+    return toReturn;
+  }
+  takeToBook(book: Book): void {
+    this.$router.push({ name: "Book", params: { id: book.id.toString() } });
+  }
+  isOverflown(): boolean {
+    const element = this.$refs.roomName as any;
+    if (element) {
+      return element!.scrollWidth > element.clientWidth;
+    }
+    return false;
+  }
+  updatePage(pageIndex: number) {
+    // m.title.toLowerCase().startsWith(this.searchText)
+    let start = (pageIndex - 1) * this.pageSize;
+    let end = pageIndex * this.pageSize;
+    this.historyList = this.filteredBooks.slice(start, end);
+    this.page = pageIndex;
+  }
+  initPage() {
+    this.listCount = this.filteredBooks.length;
+    if (this.listCount < this.pageSize) {
+      this.historyList = this.filteredBooks;
+    } else {
+      this.historyList = this.filteredBooks.slice(0, this.pageSize);
+    }
+  }
+  created() {
+    this.initPage();
+    this.updatePage(this.page);
+  }
+  updateAfterSearch() {
+    this.initPage();
+    this.updatePage(this.page);
   }
 }
 </script>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'Books' }">
+      Go To Books
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class MainHeader extends Vue {}
+</script>
