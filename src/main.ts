322d273d817ddfd25fcd114a0e3ba95a9537df80
andrej oljaca <andrejoljaca@andrejs-mbp.lan>
2022-04-12 08:14:53 -0400

    remove uneeded

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import Toast from "vue-toastification";
-import "vue-toastification/dist/index.css";
-import VueClipboard from "vue-clipboard2";
-Vue.use(Toast, {});
-Vue.use(VueClipboard);
 new Vue({
   router,
   store,
--- src/store/baseModule.ts
+++ src/store/baseModule.ts
@@ -1,23 +1,11 @@
 import { Module } from "vuex";
-import {
-    ToastContent,
-    ToastOptions,
-  } from "vue-toastification/dist/types/src/types";
 import Vue from "vue/types/umd";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const baseModule: Module<any, any> = {
   namespaced: true,
   state: {},
   mutations: {},
-  actions: {
-    sendMessage(
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      { commit },
-      config: { content: ToastContent; options: ToastOptions }
-    ) {
-      Vue.$toast(config.content, config.options);
-    },
-  },
+  actions: {},
   getters: {},
 };
 
